fb665ae323b2825c3a9e9ad7189fb04931a8e5b0
Ivaylo Hristov <ishristov@gmail.com>
2019-02-17 18:16:56 +0200

    Updates

---
+++ precache-manifest.5a3d957a8422f3e66c23652a33c0f491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f35a1bf8674396d04fef",
+    "url": "/static/js/main.f35a1bf8.chunk.js"
+  },
+  {
+    "revision": "944c16efab0c1c2a78b6",
+    "url": "/static/js/1.944c16ef.chunk.js"
+  },
+  {
+    "revision": "f35a1bf8674396d04fef",
+    "url": "/static/css/main.a5bfbddf.chunk.css"
+  },
+  {
+    "revision": "44a214fbe09e5d309952444277a5c246",
+    "url": "/index.html"
+  }
+];
